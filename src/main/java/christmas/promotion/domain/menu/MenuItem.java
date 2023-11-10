f7c585cb4a7d25cf10b309f2ec2a735e215a34aa
minseo  jo <liging12@naver.com>
2023-11-11 03:54:14 +0900

    feat: 메뉴 아이템 기능 추가

--- src/main/java/christmas/promotion/domain/menu/MenuItem.java
+++ src/main/java/christmas/promotion/domain/menu/MenuItem.java
@@ -2,43 +2,45 @@ package christmas.promotion.domain.menu;
 
 import christmas.promotion.domain.event.discount.DiscountEvent;
 
+import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
-public class MenuItem implements Menu {
+public class MenuItem {
     private final String name;
     private final double price;
+    private final String category;
     private final List<DiscountEvent> discountEvents;
 
-    public MenuItem(String name, double price, List<DiscountEvent> discountEvents) {
+    private MenuItem(String name, double price, String category, List<DiscountEvent> discountEvents) {
         this.name = name;
         this.price = price;
+        this.category = category;
         this.discountEvents = discountEvents;
     }
 
-    @Override
-    public String description() {
-        return "<메뉴>";
+    public static MenuItem createMenuItem(String name, double price, String category) {
+        return new MenuItem(name, price, category, Collections.emptyList());
     }
 
-    @Override
+    public static MenuItem createMenuItemWithDiscount(String name, double price, String category, List<DiscountEvent> discountEvents) {
+        return new MenuItem(name, price, category, discountEvents);
+    }
+
     public String getName() {
         return name;
     }
 
-    @Override
     public double getPrice() {
         return price;
     }
 
-    @Override
-    public List<DiscountEvent> getDiscountEvents() {
-        return discountEvents;
-    }
-
-    @Override
-    public void applyDiscount() {
+    public double applyDiscount(LocalDate date) {
+        double salePrice = 0;
         for (DiscountEvent discountEvent : discountEvents) {
-            discountEvent.applyDiscount(price);
+            salePrice += discountEvent.applyDiscount(date, salePrice);
         }
+        System.out.println(salePrice);
+        return salePrice;
     }
 }
