6cceeeb2132c659e92f83f6f2206537673772ce4
minseo  jo <liging12@naver.com>
2023-11-11 17:20:31 +0900

    feat: 이벤트 뱃지 기능 추가

--- src/main/java/christmas/promotion/domain/event/Badge.java
+++ src/main/java/christmas/promotion/domain/event/Badge.java
@@ -1,17 +1,34 @@
 package christmas.promotion.domain.event;
 
 public enum Badge {
-    STAR(5000.0),
-    TREE(10000.0),
-    SANTA(20000.0);
+    NONE("없음", 0.0),
+    STAR("별", 5000.0),
+    TREE("트리", 10000.0),
+    SANTA("산타", 20000.0);
 
-    private final double minimumTotalBenefit;
+    private final String name;
+    private final double threshold;
 
-    Badge(double minimumTotalBenefit) {
-        this.minimumTotalBenefit = minimumTotalBenefit;
+    Badge(String name, double threshold) {
+        this.name = name;
+        this.threshold = threshold;
     }
 
-    public double getMinimumTotalBenefit() {
-        return minimumTotalBenefit;
+    public static Badge grantBadgeOnExceedingAmountThreshold(double price) {
+        Badge badge = NONE;
+        for (Badge value : Badge.values()) {
+            if (isPriceExceedingBadgeThreshold(price, value)) {
+                badge = value;
+            }
+        }
+        return badge;
+    }
+
+    private static boolean isPriceExceedingBadgeThreshold(double price, Badge value) {
+        return price >= value.threshold;
+    }
+
+    public String getName() {
+        return name;
     }
 }
