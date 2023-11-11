d3d033d123f44e1113b52ac3104652940b61d9b8
minseo  jo <liging12@naver.com>
2023-11-11 17:24:20 +0900

    feat: 주문 parse 기능 추가

--- src/main/java/christmas/promotion/domain/order/OrderParser.java
+++ src/main/java/christmas/promotion/domain/order/OrderParser.java
@@ -1,6 +1,6 @@
 package christmas.promotion.domain.order;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,29 +8,51 @@ public class OrderParser {
     private static final String menuItemsSplitRegex = ",";
     private static final String menuDetailSplitRegex = "-";
 
-    public static Map<String, Integer> parseOrderMenus(String input) {
-        Map<String, Integer> orderItemsDetail = new HashMap<>();
+    public static Map<String, Integer> parseOrderDetails(final String input) {
+        Map<String, Integer> orderMenuDetail = new LinkedHashMap<>();
 
         List<String> menuItems = splitOrderMenus(input);
         for (String menuItem : menuItems) {
             List<String> menuDetail = splitOrderMenuDetail(menuItem);
-            parse(orderItemsDetail, menuDetail);
+            validateSize(menuDetail);
+            parse(orderMenuDetail, menuDetail);
         }
 
-        return orderItemsDetail;
+        return orderMenuDetail;
     }
-
-    private static void parse(Map<String, Integer> orderItems, List<String> menuDetail) {
-        String menuName = menuDetail.get(0);
-        int quantity = Integer.parseInt(menuDetail.get(1));
-        orderItems.put(menuName, quantity);
-    }
-
-    private static List<String> splitOrderMenus(String input) {
+    private static List<String> splitOrderMenus(final String input) {
         return List.of(input.split(menuItemsSplitRegex));
     }
 
-    private static List<String> splitOrderMenuDetail(String menuItem) {
+    private static List<String> splitOrderMenuDetail(final String menuItem) {
         return List.of(menuItem.split(menuDetailSplitRegex));
+    }
+
+    private static void validateSize(List<String> menuDetail) {
+        if (menuDetail.size() != 2) {
+            throw new IllegalArgumentException("[ERROR] 유효한 형식이 아닙니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static void parse(final Map<String, Integer> orderMenuDetail, final List<String> menuDetail) {
+        String menuName = getMenuName(menuDetail);
+        int quantity = getQuantity(menuDetail);
+        if (orderMenuDetail.containsKey(menuName)) {
+            throw  new IllegalArgumentException("[ERROR] 이미 시키신 메뉴 입니다. 다시 입력해 주세요.");
+        }
+        orderMenuDetail.put(menuName, quantity);
+    }
+
+    private static String getMenuName(List<String> menuDetail) {
+        return menuDetail.get(0);
+    }
+
+    private static int getQuantity(List<String> menuDetail) {
+        try {
+            return Integer.parseInt(menuDetail.get(1));
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효한 형식이 아닙니다. 다시 입력해 주세요.");
+        }
     }
 }
