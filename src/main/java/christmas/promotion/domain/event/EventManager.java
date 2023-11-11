82d60f952f6a77701b49989518a42646b6e1fc2c
minseo  jo <liging12@naver.com>
2023-11-11 17:22:47 +0900

    refactor: EvnetManager 기능 추가
    Order - MenuItem 에 대한 모든 이벤트를 여기서 처리한다.
    추 후 반환 값, 메소드 추출등 리팩토링 예정

--- src/main/java/christmas/promotion/domain/event/EventManager.java
+++ src/main/java/christmas/promotion/domain/event/EventManager.java
@@ -6,48 +6,87 @@ import christmas.promotion.domain.event.gift.GiftEvent;
 import christmas.promotion.domain.order.Order;
 import christmas.promotion.domain.order.OrderItem;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class EventManager {
 
     private final Order order;
     private double salePrice;
-    private final List<DiscountEvent> discountEvents;
-    private final List<GiftEvent> giftEvents;
+    private final Map<Event, Double> commonEvents;
+    private final Map<Event, Double> events;
 
     public EventManager(Order order) {
         this.order = order;
-        this.discountEvents = new ArrayList<>();
-        this.giftEvents = new ArrayList<>();
-        addEvents();
+        commonEvents = new LinkedHashMap<>();
+        events = new LinkedHashMap<>(); // 맵에 객체를 넣은 순서 유지 (크리스마스 디데이 할인, 특별 할인, 증정 이벤트)
+        addCommonEvents();
         applyEvent();
     }
 
-    private void addEvents() {
-        discountEvents.add(new ChristmasDiscount());
-        discountEvents.add(new SpecialDiscount());
-
-        giftEvents.add(new ChampagneGift());
+    private void addCommonEvents() {
+        commonEvents.put(new ChristmasDiscount(), 0.0);
+        commonEvents.put(new SpecialDiscount(), 0.0);
+        commonEvents.put(new ChampagneGift(), 0.0);
     }
 
+    public void print() {
+        for (Event event : events.keySet()) {
+            if (event.getClass() == ChristmasDiscount.class) {
+                System.out.print("크리스마 디데이 할인: ");
+            }
+            if (event.getClass().equals(WeekdayDiscount.class)) {
+                System.out.print("평일 할인:" );
+            }
+            if (event.getClass().equals(WeekendDiscount.class)) {
+                System.out.print("주말 할인: ");
+            }
+
+            if (event.getClass().equals(SpecialDiscount.class)) {
+                System.out.print("특별 할인:");
+            }
+            if (event.getClass().equals(ChampagneGift.class)) {
+                System.out.print("증정 이벤트: ");
+            }
+            System.out.println(events.getOrDefault(event, 0.0));
+        }
+
+        System.out.println(order.getBadgeName());
+    }
 
     private void applyEvent() {
-        if (order.getOriginalPrice() < 10_000) {
+        if (!order.isPriceAtLeastTenThousandWon()) {
             return;
         }
 
-        for (DiscountEvent event : discountEvents) {
-            salePrice += event.applyDiscount(order.getDate(), order.getOriginalPrice());
-        }
+        for (Event event : commonEvents.keySet()) {
+            if (event instanceof DiscountEvent) {
+                double eventSalePrice = ((DiscountEvent) event).applyDiscount(order.getDate(), order.getOriginalPrice());
+                salePrice += eventSalePrice;
+                events.put(event, eventSalePrice);
+            }
 
-        for (GiftEvent event : giftEvents) {
-            salePrice += event.applyGift(order.getDate(), order.getOriginalPrice());
+            if (event instanceof GiftEvent) {
+                double eventSalePrice = ((GiftEvent) event).applyGift(order.getDate(), order.getOriginalPrice());
+                salePrice += eventSalePrice;
+                events.put(event, eventSalePrice);
+            }
         }
 
         for (OrderItem orderItem : order.getOrder()) {
-            salePrice += orderItem.applyDiscount(order.getDate()) * orderItem.getQuantity();
+            Map<Event, Double> map = orderItem.applyDiscount(order.getDate());
+            for (Event event : map.keySet()) {
+                double eventSalePrice = map.getOrDefault(event, 0.0) * orderItem.getQuantity();
+                events.put(event, events.getOrDefault(event, 0.0) + eventSalePrice);
+                salePrice += eventSalePrice;
+            }
         }
+
+    }
+
+    public void applyEventBadge() {
+        Badge badge = Badge.grantBadgeOnExceedingAmountThreshold(salePrice);
+        order.updateEventBadge(badge);
     }
 
     public double getSalePrice() {
