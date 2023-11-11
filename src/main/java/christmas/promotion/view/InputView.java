f954c239b2dea9182f2372e35ea8bb43e1e4d550
minseo  jo <liging12@naver.com>
2023-11-11 20:19:29 +0900

    feat: InputView 기능 추가

--- src/main/java/christmas/promotion/view/InputView.java
+++ src/main/java/christmas/promotion/view/InputView.java
@@ -4,7 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private enum Message {
-        VISIT_DATE_IN_DECEMBER("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n");
+        VISIT_DATE_IN_DECEMBER("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
+        ORDER_DETAILS("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
 
         private final String message;
 
@@ -14,10 +15,19 @@ public class InputView {
     }
 
     public void requestDecemberVisitDate() {
-        System.out.print(Message.VISIT_DATE_IN_DECEMBER.message);
+        System.out.println(Message.VISIT_DATE_IN_DECEMBER.message);
     }
 
     public String readDecemberVisitDate() {
+        String decemberVisitData = Console.readLine();
+        return decemberVisitData;
+    }
+
+    public void requestOrderDetails() {
+        System.out.println(Message.ORDER_DETAILS.message);
+    }
+
+    public String readOrderDetails() {
         String decemberVisitData = Console.readLine();
         return decemberVisitData;
     }
