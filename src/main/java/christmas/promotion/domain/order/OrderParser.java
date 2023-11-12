39711b87a07d318af4318cca1937ac3be76c78d4
minseo  jo <liging12@naver.com>
2023-11-12 15:22:33 +0900

    feat: 주문 메뉴 parse, validate 기능 추가

--- src/main/java/christmas/promotion/domain/order/OrderParser.java
+++ src/main/java/christmas/promotion/domain/order/OrderParser.java
@@ -7,19 +7,24 @@ import java.util.Map;
 public class OrderParser {
     private static final String menuItemsSplitRegex = ",";
     private static final String menuDetailSplitRegex = "-";
+    private static final int menuDetailNameIndex = 0;
+    private static final int menuDetailQuantityIndex = 1;
 
-    public static Map<String, Integer> parseOrderDetails(final String input) {
-        Map<String, Integer> orderMenuDetail = new LinkedHashMap<>();
+
+    public static Map<String, Integer> parse(final String input) {
+        Map<String, Integer> orderMenuDetails = new LinkedHashMap<>();
 
         List<String> menuItems = splitOrderMenus(input);
         for (String menuItem : menuItems) {
             List<String> menuDetail = splitOrderMenuDetail(menuItem);
-            validateSize(menuDetail);
-            parse(orderMenuDetail, menuDetail);
+            OrderValidator.validateSize(menuDetail);
+            OrderValidator.validateInteger(menuDetail.get(menuDetailQuantityIndex));
+            addMenuDetail(orderMenuDetails, menuDetail);
         }
 
-        return orderMenuDetail;
+        return orderMenuDetails;
     }
+
     private static List<String> splitOrderMenus(final String input) {
         return List.of(input.split(menuItemsSplitRegex));
     }
@@ -28,31 +33,17 @@ public class OrderParser {
         return List.of(menuItem.split(menuDetailSplitRegex));
     }
 
-    private static void validateSize(List<String> menuDetail) {
-        if (menuDetail.size() != 2) {
-            throw new IllegalArgumentException("[ERROR] 유효한 형식이 아닙니다. 다시 입력해 주세요.");
-        }
-    }
-
-    private static void parse(final Map<String, Integer> orderMenuDetail, final List<String> menuDetail) {
+    private static void addMenuDetail(Map<String, Integer> orderMenuDetails, List<String> menuDetail) {
         String menuName = getMenuName(menuDetail);
         int quantity = getQuantity(menuDetail);
-        if (orderMenuDetail.containsKey(menuName)) {
-            throw  new IllegalArgumentException("[ERROR] 이미 시키신 메뉴 입니다. 다시 입력해 주세요.");
-        }
-        orderMenuDetail.put(menuName, quantity);
+        orderMenuDetails.put(menuName, quantity);
     }
 
     private static String getMenuName(List<String> menuDetail) {
-        return menuDetail.get(0);
+        return menuDetail.get(menuDetailNameIndex);
     }
 
     private static int getQuantity(List<String> menuDetail) {
-        try {
-            return Integer.parseInt(menuDetail.get(1));
-        }
-        catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 유효한 형식이 아닙니다. 다시 입력해 주세요.");
-        }
+        return Integer.parseInt(menuDetail.get(menuDetailQuantityIndex));
     }
 }
--- src/main/java/christmas/promotion/domain/order/OrderValidator.java
+++ src/main/java/christmas/promotion/domain/order/OrderValidator.java
@@ -1,4 +1,23 @@
 package christmas.promotion.domain.order;
 
+import java.util.List;
+
 public class OrderValidator {
+    private static final String ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final int menuDetailsFactorSize = 2;
+
+    static void validateSize(List<String> menuDetail) {
+        if (menuDetail.size() != menuDetailsFactorSize) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    static void validateInteger(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException exception) {
+            exception.printStackTrace();
+            throw new NumberFormatException(ERROR_MESSAGE);
+        }
+    }
 }
