5ae1fe82052409741c39a60865c8372fc8bac272
minseo  jo <liging12@naver.com>
2023-11-12 19:04:23 +0900

    feat: 주문 검증 기능 추가

--- src/main/java/christmas/promotion/domain/order/Order.java
+++ src/main/java/christmas/promotion/domain/order/Order.java
@@ -1,32 +1,28 @@
 package christmas.promotion.domain.order;
 
 import christmas.promotion.domain.event.Badge;
+import christmas.promotion.domain.menu.Beverage;
 import christmas.promotion.domain.menu.Menu;
 import christmas.promotion.domain.menu.MenuBoard;
 import christmas.promotion.dto.OrderMenusDto;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Order {
+    private static final int ORDER_MENU_MAX_SIZE = 20;
+
     private final List<OrderMenu> orderMenus;
-    private final double originalPrice;
     private final LocalDate date;
     private Badge badge;
     private final MenuBoard menuBoard;
 
     public Order(final Map<String, Integer> order, LocalDate date, final MenuBoard menuBoard) {
-        this.menuBoard = menuBoard;
-        this.date = date;
+        this.menuBoard = menuBoard; // orderMenu 하기전에 메뉴판을 동적으로 초기화 해줘야함.
         this.orderMenus = List.copyOf(createOrderFromMenuBoard(order));
-        this.originalPrice = calculateTotal();
+        validate();
+        this.date = date;
         this.badge = Badge.NONE;
-    }
-
-    public boolean isPriceAtLeastTenThousandWon() {
-        return originalPrice >= 10000;
     }
 
     public LocalDate getDate() {
@@ -35,12 +31,10 @@ public class Order {
 
     private List<OrderMenu> createOrderFromMenuBoard(Map<String, Integer> order) {
         List<OrderMenu> menus = new ArrayList<>();
-
         for (Map.Entry<String, Integer> orderMenu : order.entrySet()) {
             Menu menu = menuBoard.findMenu(orderMenu.getKey());
             menus.add(new OrderMenu(menu, orderMenu.getValue()));
         }
-
         return menus;
     }
 
@@ -48,7 +42,7 @@ public class Order {
         return orderMenus;
     }
 
-    private double calculateTotal() {
+    public double calculateTotal() {
         double total = 0.0;
         for (OrderMenu orderMenu : this.orderMenus) {
             total += orderMenu.calculateSubtotal();
@@ -56,12 +50,51 @@ public class Order {
         return total;
     }
 
-    public void updateEventBadge(Badge badge) {
-        this.badge = badge;
+    private void validate() {
+        validateMaxSize();
+        validateDuplicate();
+        validateOnlyBeverage();
     }
 
-    public double getOriginalPrice() {
-        return originalPrice;
+    private void validateMaxSize() {
+        int size = 0;
+
+        for (OrderMenu orderMenu : orderMenus) {
+            size += orderMenu.getQuantity();
+        }
+
+        if (size > ORDER_MENU_MAX_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateDuplicate() {
+        int initialSize = orderMenus.size();
+
+        Set<OrderMenu> uniqueMenu = new HashSet<>(orderMenus);
+        int uniqueMenuSize = uniqueMenu.size();
+
+        if (initialSize != uniqueMenuSize) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateOnlyBeverage() {
+        for (OrderMenu orderMenu : orderMenus) {
+            if (isNotBeverage(orderMenu.getMenu())) {
+                return;
+            }
+        }
+
+        throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    private boolean isNotBeverage(Menu menu) {
+        return !(menu instanceof Beverage) || menu.getClass() != Beverage.class;
+    }
+
+    public void updateEventBadge(Badge badge) {
+        this.badge = badge;
     }
 
     public String getBadgeName() {
