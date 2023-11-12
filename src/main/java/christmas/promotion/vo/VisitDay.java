51182b12f8e414d3ce99f9d879e286bac59b9d44
minseo  jo <liging12@naver.com>
2023-11-12 16:57:30 +0900

    refactor: 상수로 변경

--- src/main/java/christmas/promotion/vo/VisitDay.java
+++ src/main/java/christmas/promotion/vo/VisitDay.java
@@ -1,8 +1,10 @@
 package christmas.promotion.vo;
 
 public class VisitDay {
-    //Integer 는 -128 ~ 127까지 캐싱 되므로, 1 ~ 31일은 캐싱 데이터를 안 만들었다.
+    private static final int DECEMBER_START_DAY = 1;
+    private static final int DECEMBER_END_DAY = 31;
 
+    //Integer 는 -128 ~ 127까지 캐싱 되므로, 1 ~ 31일은 캐싱 데이터를 안 만들었다.
     private final Integer visitDay;
 
     public VisitDay(String visitDay) {
@@ -18,7 +20,7 @@ public class VisitDay {
         }
 
         int value = Integer.parseInt(visitDay);
-        if (value < 1 || value > 31) {
+        if (value < DECEMBER_START_DAY || value > DECEMBER_END_DAY) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
