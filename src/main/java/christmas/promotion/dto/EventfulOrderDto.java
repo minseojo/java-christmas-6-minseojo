5cfc42f9cc2c5a1e8d3973a424112555c6ca4484
minseo  jo <liging12@naver.com>
2023-11-13 18:49:02 +0900

    feat: eventfulOrder - DTO 기능 추가

---
+++ src/main/java/christmas/promotion/dto/EventfulOrderDto.java
@@ -0,0 +1,61 @@
+package christmas.promotion.dto;
+
+import christmas.promotion.domain.event.Event;
+import christmas.promotion.domain.event.badge.Badge;
+import christmas.promotion.domain.menu.Menu;
+import christmas.promotion.domain.order.OrderMenu;
+import christmas.promotion.vo.Price;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class EventfulOrderDto {
+    private final List<OrderMenu> orderMenus; // 주문 메뉴
+    private final Price originalPrice; // 할인 전 총 주문 금액
+    private final Map<Menu, Integer> giftMenus; // 증정 메뉴
+    private final Map<Event, Price> eventBenefits; // 혜택 내역
+    private final Price totalBenefitPrice; // 총 혜택 금액
+    private final Price discountedPrice; // 할인 후 예상 결제 금액
+    private final Badge badge; // 배지
+
+    public EventfulOrderDto(List<OrderMenu> orderMenus, Price originalPrice, Map<Menu, Integer> giftMenus,
+                            Map<Event, Price> eventBenefits, Price totalBenefitPrice,
+                            Price discountedPrice, Badge badge) {
+        this.orderMenus = List.copyOf(orderMenus);
+        this.originalPrice = originalPrice;
+        this.giftMenus = Collections.unmodifiableMap(giftMenus);
+        this.eventBenefits = Collections.unmodifiableMap(eventBenefits);
+        this.totalBenefitPrice = totalBenefitPrice;
+        this.discountedPrice = discountedPrice;
+        this.badge = badge;
+    }
+
+    public OrderMenusDto getOrderMenus() {
+        return new OrderMenusDto(orderMenus);
+    }
+
+    public Price getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public GiftMenusDto getGiftMenus() {
+        return new GiftMenusDto(giftMenus);
+    }
+
+    public EventBenefitsDto getEventBenefits() {
+        return new EventBenefitsDto(eventBenefits);
+    }
+
+    public Price getTotalBenefitPrice() {
+        return totalBenefitPrice;
+    }
+
+    public Price getDiscountedPrice() {
+        return discountedPrice;
+    }
+
+    public Badge getBadge() {
+        return badge;
+    }
+}
